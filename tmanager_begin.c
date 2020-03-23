a4187dac46cabe80a3dfe546387c745851ef537a
ctman96 <ctman96@students.cs.ubc.ca>
2020-03-23 15:03:10 -0700

    Initial begin and join code

--- tmanager_begin.c
+++ tmanager_begin.c
@@ -3,7 +3,35 @@
 //
 // Handler code for Begin
 //
+#include "tmanager.h"
+#include "transaction_msg.h"
 
 int begin(struct transactionSet * txlog, struct txMsgType message, struct sockaddr_in client) {
-    // TODO
+    // Check valid request (TID doesn't conflict and can handle it)
+    int t = -1;
+    int id_conflict = 0;
+    for (int i = 0; i < MAX_TX; i++) {
+        if (txlog->transaction[i].tstate == TX_NOTINUSE)
+            t = i;
+        else if (txlog->transaction[i].tstate != TX_NOTINUSE && txlog->transaction[i].txID == message.tid)
+            id_conflict = 1;
+    }
+
+    if (t == -1 || id_conflict) {
+        // TODO reply failure
+        return 0;
+    }
+
+    // Begin transaction
+    txlog->transaction[t].txID = message.tid;
+    for (int i = 0; i < MAX_WORKERS; i++) {
+        txlog->transaction[t].worker[i].sin_port = 0; // Treat port 0 default/unassigned
+    }
+    txlog->transaction[t].worker[0] = client;
+    txlog->transaction[t].tstate = TX_INPROGRESS;
+
+    // TODO Reply Success
+    // TODO Logging
+
+    return 0;
 }
--- tmanager_join.c
+++ tmanager_join.c
@@ -5,5 +5,39 @@
 //
 
 int join(struct transactionSet * txlog, struct txMsgType message, struct sockaddr_in client) {
-    // TODO
+    // Check valid request (TID exists)
+    int t = -1;
+    for (int i = 0; i < MAX_TX; i++) {
+        if (txlog->transaction[i].tstate != TX_NOTINUSE && txlog->transaction[i].txID == message.tid)
+            t = i;
+    }
+
+    if (t == -1) {
+        // TODO reply failure
+        return 0;
+    }
+
+    // Check for space or already existing
+    txlog->transaction[t].txID = message.tid;
+    int w = -1;
+    for (int i = 0; i < MAX_WORKERS; i++) {
+        if (txlog->transaction[t].worker[i].sin_port == 0) { // Treat port 0 default/unassigned
+            w = i;
+        }
+        if (txlog->transaction[t].worker[i].sin_port == client.sin_port) {
+            w = i; // Counting already being joined as success
+            break;
+        }
+    }
+    if (w == -1) {
+        // TODO reply failure
+        return 0;
+    }
+    // Add worker to transaction
+    txlog->transaction[t].worker[w] = client;
+
+    // TODO Reply Success
+    // TODO Logging
+
+    return 0;
 }
